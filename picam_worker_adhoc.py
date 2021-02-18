4dea49ee7c7451c4659ba17f1efb77e6b4d5d200
brian-burton <brian-burton@users.noreply.github.com>
2021-02-18 20:20:07 +0000

    refactor(worker): Rewrite in good form
    Rewrite and modularise to the best of Brian's Python ability

--- picam_worker_adhoc.py
+++ picam_worker_adhoc.py
@@ -7,6 +7,7 @@ import os
 import argparse
 
 from syslog import syslog
+from sys import exit
 
 def arghandler():
     parser = argparse.ArgumentParser(description='Take a single picture with the Pi Camera',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -17,43 +18,49 @@ def arghandler():
     parser.add_argument('-r', '--rotation', default = 0, type=int, help='rotation of the output image (0, 90, 180, 270)')
     return parser.parse_args()
 
-def multishot():
-    """Take multiple shots, keeping the camera running between frames for faster framerates"""
+def taketheshot(width: int, height: int, rotation: int, interval: float, duration: int):
+    """Take multiple shots. If the interval between shots is long, shutdown to save power"""
+    shots = math.ceil(duration/interval)
+    series = math.floor(time.time() * 1000)
+    os.makedirs('/data/' + str(series), exist_ok=True)
+    KEEPCAMHOT = True if (interval < 5) else False
+    WARMUPTIME = 2
+
     with picamera.PiCamera() as camera:
-        camera.rotation = ROTATION
-        camera.resolution = (CAMERA_RES_X, CAMERA_RES_Y)
+        camera.rotation = rotation
+        camera.resolution = (width, height)
         #  camera.awb_mode='off'
         #  camera.iso=400
         camera.led=False
-        camera.start_preview(resolution = (1920,1080))
-        time.sleep(2)
-        try:
-            series = math.floor(time.time() * 1000)
-            os.makedirs('/data/' + str(series), exist_ok=True)
-            for i, filename in enumerate(camera.capture_continuous('/data/' + str(series) + '/{counter:06d}.jpg',format='jpeg',quality=80)):
-                #print(filename + ' out of ' + str(NUM_SHOTS))
-                time.sleep(1/CAMERA_FPS)
-                if i == NUM_SHOTS:
-                    break
-        finally:
-            camera.stop_preview()
+
+        if KEEPCAMHOT:
+            camera.start_preview(resolution = (width,height))
+            time.sleep(WARMUPTIME)
+            try:
+                for i, filename in enumerate(camera.capture_continuous('/data/' + str(series) + '/{counter:06d}.jpg',format='jpeg',quality=80)):
+                    if i == shots:
+                        break
+                    time.sleep(interval)
+            finally:
+                camera.stop_preview()
+        else:
+            for i in range(shots + 1):
+                camera.start_preview(resolution = (width,height))
+                time.sleep(WARMUPTIME)
+                camera.capture('/data/' + str(series) + f'/{i:06d}.jpg',format='jpeg',quality=80)
+                camera.stop_preview()
+                if i < shots:
+                    time.sleep(interval - WARMUPTIME)
 
 def main():
     args = arghandler()
-    print(args)
-    ROTATION = args.rotation
-    CAMERA_RES_X = args.width
-    CAMERA_RES_Y = args.height
-    CAMERA_FPS = 1 / args.interval
-    DURATION = args.duration
-    NUM_SHOTS = (DURATION * CAMERA_FPS)
-    KEEPCAMHOT = True if (args.interval < 5) else False
-
-    if KEEPCAMHOT:
-        multishot()
-#    else:
-        # TODO: function to turn cam off between shots
-
+    #syslog(args)
+    #print(args)
+    if args.duration < args.interval:
+        #syslog(f"Duration ({args.duration}s) cannot be less than interval between shots ({args.interval}s)")
+        print(f"Duration ({args.duration}s) cannot be less than interval between shots ({args.interval}s)")
+        exit(1)
+    taketheshot(args.width, args.height, args.rotation, args.interval, args.duration)
 
 if __name__ == "__main__":
     main()
